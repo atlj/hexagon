5517e565c4a8e8245d07cc388561ce276ac9480a
Burak <md.burak.guner@gmail.com>
2022-06-19 16:44:15 +0300

    feat: install and configure tailwind

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,5 @@
+<script>
+	import '../style/index.scss';
+</script>
+
+<slot />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="/blog/test">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Visit <a class="text-green-400" href="/blog/test">kit.svelte.dev</a> to read the documentation
+</p>
