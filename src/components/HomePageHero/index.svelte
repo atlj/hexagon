cf5bf777378c15a277f3aba6f5204784ed08dc8a
Burak <md.burak.guner@gmail.com>
2022-06-22 23:20:09 +0300

    feat: add target _blank to homepage hero

--- src/components/HomePageHero/index.svelte
+++ src/components/HomePageHero/index.svelte
@@ -8,11 +8,18 @@
 		<span class="hero__name--title"> FRONTEND DEV </span>
 	</div>
 	<div class="hero__links">
-		<p>check my code on <span><a href="https://github.com/atlj">Github</a></span></p>
-		<p>see what i said recently <span><a href="https://twitter.com/_atlj">Twitter</a></span></p>
+		<p>
+			check my code on <span><a href="https://github.com/atlj" target="_blank">Github</a></span>
+		</p>
+		<p>
+			see what i said recently <span
+				><a href="https://twitter.com/_atlj" target="_blank">Twitter</a></span
+			>
+		</p>
 		<p>
 			business stuff <span
-				><a href="https://linkedin.com/in/burak-g%C3%BCner-55a906222/">LinkedIn</a></span
+				><a href="https://linkedin.com/in/burak-g%C3%BCner-55a906222/" target="_blank">LinkedIn</a
+				></span
 			>
 		</p>
 		<p>or just <span><a href="mailto:md.burak.guner@gmail.com">mail</a></span> me</p>
