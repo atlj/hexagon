c3c096021c4e6d9dea8bd9a011cd95d692ef9536
Burak <md.burak.guner@gmail.com>
2022-06-22 21:34:16 +0300

    feat: theme store

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,28 @@
 <script>
 	import '../style/index.scss';
+	import { theme } from '@/store/theme.svelte';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		theme.subscribe((value) => {
+			const body = document.body;
+			if (value === 'dark') {
+				body.classList.add('dark');
+				body.style.backgroundColor = '#1C1A1D';
+			} else {
+				body.classList.remove('dark');
+				body.style.backgroundColor = 'white';
+			}
+		});
+
+		theme.subscribe(function writeToLocalStorage(newValue) {
+			localStorage.setItem('theme', newValue);
+		});
+	});
 </script>
 
 <svelte:head />
-<slot />
+
+<div>
+	<slot />
+</div>
---
+++ src/store/theme.svelte
@@ -0,0 +1,10 @@
+<script lang="ts" context="module">
+	import { writable } from 'svelte/store';
+
+	export const theme = writable<'dark' | 'light'>('dark', (set) => {
+		const savedTheme = localStorage.getItem('theme');
+		if (savedTheme === 'dark' || savedTheme === 'light') {
+			set(savedTheme);
+		}
+	});
+</script>
