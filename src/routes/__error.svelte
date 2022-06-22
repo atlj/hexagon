7a47f80df153c8c5f33127c92d28f54c460a82e8
Burak <md.burak.guner@gmail.com>
2022-06-22 23:15:34 +0300

    feat: add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" context="module">
+	export function load({ status }: { status: number }) {
+		return {
+			props: {
+				message: `${status}`
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let message: string;
+</script>
+
+<div class="error">
+	{message}
+</div>
