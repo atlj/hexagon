3bab87165076f2fb898cda3e2403e3bb327e604f
Burak <md.burak.guner@gmail.com>
2022-07-05 21:44:30 +0300

    feat: add custom hmr to handle changes on markdown files

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,24 @@ import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import path from 'path';
 
+function handleMarkdownChanges() {
+	return {
+		name: 'custom-hmr',
+		enforce: 'post',
+		// HMR
+		handleHotUpdate({ file, server }) {
+			if (file.endsWith('.md')) {
+				console.log('md file changed');
+
+				server.ws.send({
+					type: 'full-reload',
+					path: '*'
+				});
+			}
+		}
+	};
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess({
@@ -14,6 +32,7 @@ const config = {
 			default: true
 		},
 		vite: {
+			plugins: [handleMarkdownChanges()],
 			resolve: {
 				alias: {
 					'@': path.resolve('./src')
